e1a9f5cbadb4a073f7eda00ca99e081e1bce7c46
Ravi Giroti <rgiroti@ebay.com>
2011-08-03 13:03:07 -0700

    Initial commit

---
+++ src/HelloGitWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author: rgiroti
+ * @created: 8/3/11 - 11:26 AM
+ */
+
+public class HelloGitWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git World");
+    }
+
+
+}
